9c685434d857f286b8ef19084e931715f1b3232a
jfhr <me@jfhr.de>
2021-04-16 20:48:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# writing-prompt-generator
+
+## What does it do?
+
+It generates random writing prompts
+
+## How does it do it?
+
+Simple recursive substition algorithm, look at `index.js`
---
+++ index.js
@@ -0,0 +1,183 @@
+class WritingPromptGenerator {
+    tokens = {
+        templates: [
+            'Write about the !relationship between !character and !character',
+            'Write a !genre about the !relationship between !character and !character',
+            'Write about the !relationship between !character and their !attribute companion',
+            'Write a !genre about the !relationship between !character and their !attribute companion',
+            'Write about !character in !setting',
+            'Write a !genre about !character in !setting',
+            'Write about !character and !character in !setting',
+            'Write a !genre about !character and !character in !setting',
+            'Write about !character and !character in !setting',
+            'Write a !genre about !character and !character in !setting',
+            'Write a !genre set in !setting',
+        ],
+        character: [
+            'a !attribute !characterType',
+            'a !characterType',
+        ],
+        attribute: [
+            'bold',
+            'timid',
+            'kind',
+            'lazy',
+            'intelligent',
+            'clueless',
+            'uncharismatic',
+            'wise',
+            'stupid',
+            'dexterous',
+            'clumsy',
+            'strong',
+            'weak',
+            'flexible',
+            'stiff',
+            'creative',
+            'careless',
+            'enthusiastic',
+            'apathetic',
+            'bubbly',
+            'monotone',
+            'cheerful',
+            'melancholic',
+            'optimistic',
+            'pessimistic',
+            'calm',
+            'hot-headed',
+            'brave',
+            'cowardly',
+            'patient',
+            'cautious',
+            'spineless',
+            'playful',
+            'serious',
+            'mature',
+            'immature',
+            'introverted',
+            'extroverted',
+            'naive',
+        ],
+        characterType: [
+            'artist',
+            'architect',
+            'dancer',
+            'musician',
+            'photographer',
+            'writer',
+            'blacksmith',
+            'engineer',
+            'mechanic',
+            'construction worker',
+            'CEO',
+            'scientist',
+            'doctor',
+            'veterinarian',
+            'astronaut',
+            'historian',
+        ],
+        genre: [
+            'fantasy story',
+            'sci-fi story',
+            'comedy',
+            'romantic story',
+            'romcom',
+            'historical drama',
+            'ya story',
+            'thriller',
+            'horror story',
+        ],
+        relationship: [
+            'friendship',
+            'relationship',
+            'rivalship',
+        ],
+        setting: [
+            'the middle ages',
+            'the renaissance',
+            'prehistoric times',
+            'the ming dynasty',
+            'the 1920s',
+            'the 1960s',
+            'the 1970s',
+            'east berlin',
+            'brazil',
+            'russia',
+            'china',
+            'the andromeda galaxy',
+            'virtual reality',
+            'a nuclear bunker',
+            'highschool',
+            'university',
+        ]
+    }
+
+    generate() {
+        const template = this.randomChoice(this.tokens.templates);
+        return this.substituteTokens(template);
+    }
+
+    substituteTokens(value) {
+        const output = [];
+        const choices = [];
+        let pushA = false;
+
+        for (const token of value.split(' ')) {
+            if (token.startsWith('!')) {
+                const insertTokenType = token.substr(1);
+                const insert = this.randomChoiceExcept(this.tokens[insertTokenType], choices);
+                choices.push(insert);
+                const subst = this.substituteTokens(insert);
+                if (pushA) {
+                    if (this.startsWithVowel(subst)) {
+                        output.push('an');
+                    } else {
+                        output.push('a');
+                    }
+                    pushA = false;
+                }
+                output.push(subst);
+            } else if (token === 'a') {
+                pushA = true;
+            } else {
+                output.push(token);
+                pushA = false;
+            }
+        }
+
+        return output.join(' ');
+    }
+
+    startsWithVowel(value) {
+        return value.startsWith('a')
+            || value.startsWith('e')
+            || value.startsWith('i')
+            || value.startsWith('o')
+            || value.startsWith('u');
+    }
+
+    randomChoiceExcept(values, exclude) {
+        let result;
+        do {
+            result = this.randomChoice(values);
+        } while (exclude.indexOf(result) !== -1);
+        return result;
+    }
+
+    randomChoice(values) {
+        const index = Math.floor(Math.random() * values.length);
+        return values[index];
+    }
+}
+
+window.addEventListener('load', () => {
+    const promptElement = document.getElementById('promptElement');
+    const promptButton = document.getElementById('promptButton');
+    const generator = new WritingPromptGenerator();
+
+    promptButton.addEventListener('click', () => {
+        promptElement.innerText = generator.generate();
+    });
+    
+    promptElement.innerText = generator.generate();
+});

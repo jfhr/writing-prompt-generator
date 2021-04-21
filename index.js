1738e884ed8c3f0ee9768f11d31d17593f620557
jfhr <me@jfhr.de>
2021-04-21 10:01:50 +0200

    Simplify core logic

--- index.js
+++ index.js
@@ -1,140 +1,138 @@
 class WritingPromptGenerator {
-    tokens = {
-        templates: [
-            'Write about the !relationship between !character and !character',
-            'Write a !genre about the !relationship between !character and !character',
-            'Write about the !relationship between !character and their !attribute companion',
-            'Write a !genre about the !relationship between !character and their !attribute companion',
-            'Write about !character in !setting',
-            'Write a !genre about !character in !setting',
-            'Write about !character and !character in !setting',
-            'Write a !genre about !character and !character in !setting',
-            'Write about !character and !character in !setting',
-            'Write a !genre about !character and !character in !setting',
-            'Write a !genre set in !setting',
-        ],
-        character: [
-            'a !attribute !characterType',
-            'a !characterType',
-        ],
-        attribute: [
-            'bold',
-            'timid',
-            'kind',
-            'lazy',
-            'intelligent',
-            'clueless',
-            'uncharismatic',
-            'wise',
-            'stupid',
-            'dexterous',
-            'clumsy',
-            'strong',
-            'weak',
-            'flexible',
-            'stiff',
-            'creative',
-            'careless',
-            'enthusiastic',
-            'apathetic',
-            'bubbly',
-            'monotone',
-            'cheerful',
-            'melancholic',
-            'optimistic',
-            'pessimistic',
-            'calm',
-            'hot-headed',
-            'brave',
-            'cowardly',
-            'patient',
-            'cautious',
-            'spineless',
-            'playful',
-            'serious',
-            'mature',
-            'immature',
-            'introverted',
-            'extroverted',
-            'naive',
-        ],
-        characterType: [
-            'artist',
-            'architect',
-            'dancer',
-            'musician',
-            'photographer',
-            'writer',
-            'blacksmith',
-            'engineer',
-            'mechanic',
-            'construction worker',
-            'CEO',
-            'scientist',
-            'doctor',
-            'veterinarian',
-            'astronaut',
-            'historian',
-        ],
-        genre: [
-            'fantasy story',
-            'sci-fi story',
-            'comedy',
-            'romantic story',
-            'romcom',
-            'historical drama',
-            'YA story',
-            'thriller',
-            'horror story',
-        ],
-        relationship: [
-            'friendship',
-            'relationship',
-            'rivalship',
-        ],
-        setting: [
-            'the middle ages',
-            'the renaissance',
-            'prehistoric times',
-            'the ming dynasty',
-            'the 1920s',
-            'the 1960s',
-            'the 1970s',
-            'east berlin',
-            'brazil',
-            'russia',
-            'china',
-            'the andromeda galaxy',
-            'virtual reality',
-            'a nuclear bunker',
-            'highschool',
-            'university',
-        ]
+    getTokens() {
+        return {
+            template: [
+                'Write about the !relationship between !character and !character',
+                'Write a !genre about the !relationship between !character and !character',
+                'Write about the !relationship between !character and their !attribute companion',
+                'Write a !genre about the !relationship between !character and their !attribute companion',
+                'Write about !character in !setting',
+                'Write a !genre about !character in !setting',
+                'Write about !character and !character in !setting',
+                'Write a !genre about !character and !character in !setting',
+                'Write about !character and !character in !setting',
+                'Write a !genre about !character and !character in !setting',
+                'Write a !genre set in !setting',
+            ],
+            character: [
+                'a !attribute !characterType',
+                'a !characterType',
+            ],
+            attribute: [
+                'bold',
+                'timid',
+                'kind',
+                'lazy',
+                'intelligent',
+                'clueless',
+                'uncharismatic',
+                'wise',
+                'stupid',
+                'dexterous',
+                'clumsy',
+                'strong',
+                'weak',
+                'flexible',
+                'stiff',
+                'creative',
+                'careless',
+                'enthusiastic',
+                'apathetic',
+                'bubbly',
+                'monotone',
+                'cheerful',
+                'melancholic',
+                'optimistic',
+                'pessimistic',
+                'calm',
+                'hot-headed',
+                'brave',
+                'cowardly',
+                'patient',
+                'cautious',
+                'spineless',
+                'playful',
+                'serious',
+                'mature',
+                'immature',
+                'introverted',
+                'extroverted',
+                'naive',
+            ],
+            characterType: [
+                'artist',
+                'architect',
+                'dancer',
+                'musician',
+                'photographer',
+                'writer',
+                'blacksmith',
+                'engineer',
+                'mechanic',
+                'construction worker',
+                'CEO',
+                'scientist',
+                'doctor',
+                'veterinarian',
+                'astronaut',
+                'historian',
+            ],
+            genre: [
+                'fantasy story',
+                'sci-fi story',
+                'comedy',
+                'romantic story',
+                'romcom',
+                'historical drama',
+                'YA story',
+                'thriller',
+                'horror story',
+            ],
+            relationship: [
+                'friendship',
+                'relationship',
+                'rivalship',
+            ],
+            setting: [
+                'the middle ages',
+                'the renaissance',
+                'prehistoric times',
+                'the ming dynasty',
+                'the 1920s',
+                'the 1960s',
+                'the 1970s',
+                'east berlin',
+                'brazil',
+                'russia',
+                'china',
+                'the andromeda galaxy',
+                'virtual reality',
+                'a nuclear bunker',
+                'highschool',
+                'university',
+            ]
+        };
     }
 
     generate() {
-        const template = this.randomChoice(this.tokens.templates);
-        return this.substituteTokens(template);
+        const subTokens = this.getTokens();
+        return this.substituteTokens('!template', subTokens);
     }
 
-    substituteTokens(value, exclude = null) {
-        if (exclude === null) {
-            exclude = [];
-        }
+    substituteTokens(value, subTokens) {
         const output = [];
         let pushA = false;
 
         for (const token of value.split(' ')) {
             if (token.startsWith('!')) {
-                const insertTokenType = token.substr(1);
-                const insert = this.randomChoiceExcept(this.tokens[insertTokenType], exclude);
-                exclude.push(insert);
-                const subst = this.substituteTokens(insert, exclude);
+                const tokenType = token.substr(1);
+                const subToken = this.randomChoiceAndRemove(subTokens[tokenType]);
+                const subValue = this.substituteTokens(subToken, subTokens);
                 if (pushA) {
-                    output.push(this.getAOrAn(subst));
+                    output.push(this.getAOrAn(subValue));
                     pushA = false;
                 }
-                output.push(subst);
+                output.push(subValue);
             } else if (token === 'a') {
                 pushA = true;
             } else {
@@ -161,17 +159,11 @@ class WritingPromptGenerator {
             || value.startsWith('u');
     }
 
-    randomChoiceExcept(values, exclude) {
-        let result;
-        do {
-            result = this.randomChoice(values);
-        } while (exclude.indexOf(result) !== -1);
-        return result;
-    }
-
-    randomChoice(values) {
+    randomChoiceAndRemove(values) {
         const index = Math.floor(Math.random() * values.length);
-        return values[index];
+        const value = values[index];
+        values.splice(index, 1);
+        return value
     }
 }
 
@@ -183,6 +175,6 @@ window.addEventListener('load', () => {
     promptButton.addEventListener('click', () => {
         promptElement.innerText = generator.generate();
     });
-    
+
     promptElement.innerText = generator.generate();
 });
